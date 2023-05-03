27253eaa5150e40ceb9f8681f44bb313d4a1931c
고동환\eurdr <eurdream98@naver.com>
2023-05-03 16:43:15 +0900

    [문제 1 해결]5개의 메소드를 이용했습니다.

---
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.level01.basic;
+
+public class Calculator {
+    public void checkMethod(){
+        System.out.println("메소드 호출 확인");
+    }
+    public int sum1to10(){
+    int sum = 0;
+        for(int i = 1;i<11;i++){
+            sum=sum+i;
+        }
+        return sum;
+
+
+    }
+    public int checkMaxNumber(int a,int b){
+        int result=0;
+        if(a<b){
+            result=b;
+        }if(a>b){
+            result=a;
+        }if(a==b){
+            result=a;
+        }
+        return result;
+
+    }
+
+    public int sumTwoNumber(int a, int b){
+        return a+b;
+    }
+
+    public int minusTwoNumber(int a, int b){
+        return (a-b);
+    }
+
+
+}
