4af535e0a23d290066609dbed741e901950d2232
고동환\eurdr <eurdream98@naver.com>
2023-05-04 08:57:27 +0900

    [문제 1 해결]5개의 메소드를 이용했습니다.

--- src/main/java/com/ohgiraffers/level01/basic/Application.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application.java
@@ -1,7 +1,7 @@
 package com.ohgiraffers.level01.basic;
 
 public class Application {
-    
+
     public static void main(String[] args) {
         Calculator c = new Calculator();
         c.checkMethod();
--- src/main/java/com/ohgiraffers/level01/basic/Calculator.java
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -24,6 +24,7 @@ public class Calculator {
         }
         return result;
 
+
     }
 
 
